a341147072c488fa67ebbb35a32d9d1da3dc3a9e
Derrick Shibero Wakhu <shiberoderrickwakhu@gmail.com>
2025-04-06 13:29:56 +0300

    creating a form for inserting data

--- todo-frontend/src/Task.jsx
+++ todo-frontend/src/Task.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 function Task() {
   const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   useEffect(function () {
     const fetchData = function () {
       fetch("http://localhost:5000/tasks")
@@ -16,20 +18,45 @@ function Task() {
 
   if (tasks === null) return <p>Loading tasks...</p>;
 
-  if (tasks.length === 0) return <p>No tasks found.</p>;
   return (
     <>
       <div>
-        <h1>tasks</h1>
-        {tasks.length === 0 ? (
-          <p>No Tasks currently available</p>
-        ) : (
-          <ul>
-            {tasks.map((task) => (
-              <li key={task._id}>{task._id}</li>
-            ))}
-          </ul>
-        )}
+        <h1 className="text-4x1 underline">Tasks</h1>
+        <div>
+          <h2>Add new task</h2>
+          <form action="" method="post">
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Title of the tasks"
+              value={title}
+              onChange={(e)=> setTitle(e.target.value)}
+            />
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="2"
+              value={description}
+              onChange={(e)=> setDescription(e.target.value)}
+              placeholder="Description"
+            ></textarea>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+        <div>
+          <h2>All Tasks</h2>
+          {tasks.length === 0 ? (
+            <p>No Tasks currently available</p>
+          ) : (
+            <ul>
+              {tasks.map((task) => (
+                <li key={task._id}>{task._id}</li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </>
   );
