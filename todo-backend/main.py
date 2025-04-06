a06888b37949a316402255119177a19311d6441c
Derrick Shibero Wakhu <shiberoderrickwakhu@gmail.com>
2025-04-06 12:42:13 +0300

    adding code to access mongodb

--- todo-backend/main.py
+++ todo-backend/main.py
@@ -1,10 +1,20 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource
+from pymongo import MongoClient
+from bson import ObjectId
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
+# MongoDB Connection Details
+MONGO_URI = "mongodb://localhost:27017/"  # Replace with your MongoDB URI
+DATABASE_NAME = "todo"
+
+client = MongoClient(MONGO_URI)
+db = client[DATABASE_NAME]
+task_collection = db["tasks"]
 
 @api.resource("/")
 class Hello(Resource):
@@ -18,6 +28,11 @@ class HelloItem(Resource):
             "message": "Hello item",
             "item": item
         }
+    
+@api.resource("/tasks")
+class Tasks(Resource):
+    def get(self):
+        return {"route":"/tasks", "data":"data"}
 
 if __name__ == "__main__":
     app.run()
